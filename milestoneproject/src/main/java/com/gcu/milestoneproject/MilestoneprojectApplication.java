26e4b1c96110679819a3ea6fb09a26f335098144
YoselinG01 <yosiarizona484@gmail.com>
2025-09-12 13:57:17 -0700

    Add files via upload
    Added Milestone Project

---
+++ milestoneproject/src/main/java/com/gcu/milestoneproject/MilestoneprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gcu.milestoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneprojectApplication.class, args);
+	}
+
+}
---
+++ milestoneproject/src/test/java/com/gcu/milestoneproject/MilestoneprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.milestoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilestoneprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
